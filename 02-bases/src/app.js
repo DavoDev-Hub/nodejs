623f75a6c0523f5e0a6f3f7e3783bdeceb676560
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-28 22:23:56 -0600

    Agregar plugins get-age y get-id, y actualizar package.json y package-lock.json

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,16 +1,31 @@
 // const {emailTemplate} = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
 //const {getUserById} = require('./js-foundation/03-callbacks');
-const {getUserById} = require('./js-foundation/04-arrow');
+// const {getUserById} = require('./js-foundation/04-arrow');
+const { getAge,getUUID } = require("./plugins");
 
-const id = 2
+const {buildMakePerson} = require('./js-foundation/05-factory');
 
-getUserById(id, ( error , user) =>{ 
-    if( error ){
-        throw new Error(error);
-    }
-    console.log({user});
-});
+const makePerson = buildMakePerson({ getUUID, getAge})
+
+const obj = { name:'John', birthdate: '2004-01-13' }
+
+ const john = makePerson( obj )
+
+ console.log(john)
+
+
+
+
+
+// const id = 2
+
+// getUserById(id, ( error , user) =>{ 
+//     if( error ){
+//         throw new Error(error);
+//     }
+//     console.log({user});
+// });
 
 
 //console.log(emailTemplate)
