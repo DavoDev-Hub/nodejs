54dc27d10c85d5738dcca91da0a1a357c4dad613
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-31 20:17:04 -0600

    feat: actualizar los timestamps en los informes de cobertura y agregar pruebas para la plantilla de correo

--- 04-Testing/src/test/js-foundation/01-template.test.ts
+++ 04-Testing/src/test/js-foundation/01-template.test.ts
@@ -5,4 +5,9 @@ describe("js-foundation/01-template", () => {
   test("emailTemplate should contain a greeting", () => {
     expect(emailTemplate).toContain("Hi, ");
   });
+
+  test("emailTemplate should contain {{name}} and {{orderId}}", () => {
+    expect(emailTemplate).toMatch(/{{name}}/);
+    expect(emailTemplate).toMatch(/{{orderId}}/);
+  });
 });
