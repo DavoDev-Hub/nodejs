7a4373ed0ac975fd1a4c05aac00f61d36b51ddba
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-01 19:25:46 -0600

    feat: initialize multiplication project with TypeScript setup
    - Added package.json with necessary scripts and devDependencies.
    - Created app.ts to generate multiplication table and save to file.
    - Configured tsconfig.json for TypeScript compilation settings.

---
+++ 05-multiplication/src/app.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+
+const message: string = "Hola Mundo!";
+
+const base = 5;
+let outputMessage = "";
+
+const headerMessage = `
+=========================
+     Tabla del ${base}
+=========================
+`;
+for (let i = 1; i < 10; i++) {
+  outputMessage += `${base} x ${i} = ${base * i}\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+console.log(outputMessage);
+
+const outputPath = `output`;
+
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+console.log("File created");
