226944fc447f532b786aba4263052a14a0e54b92
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-29 19:26:38 -0600

    feat: agregar funcionalidad para obtener información del archivo en el sistema de archivos

--- CNodeDC/2.filesystem.js
+++ CNodeDC/2.filesystem.js
@@ -1,1 +1,13 @@
 // fs significa file system
+
+const fs = require('node:fs'); // Nos permite trabajar con el sistema de archivos
+
+const stat = fs.statSync('CNodeDC/archivo.txt'); // Nos da información del archivo
+
+console.log('Información del archivo:');
+console.log(
+    stat.isFile(), // si es un fichero
+    stat.isDirectory(), // si es un directorio
+    stat.isSymbolicLink(), // si es un enlace simbólico
+    stat.size, // tamaño en bytes
+)
