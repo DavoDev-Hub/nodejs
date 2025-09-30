3b228e63bb13f90e3e7d1d2ad50baf045f1defc5
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-29 19:34:39 -0600

    feat: agregar funcionalidad para leer archivos de forma síncrona y asíncrona

--- CNodeDC/3.fs-readFile.js
+++ CNodeDC/3.fs-readFile.js
@@ -1,4 +1,15 @@
 const fs = require('node:fs'); // Nos permite trabajar con el sistema de archivos
 
+console.log('Leyendo el primer archivo...:');
 const text = fs.readFileSync('CNodeDC/archivo.txt', 'utf-8'); // Leemos el archivo de forma síncrona
 console.log(text);
+
+
+console.log('Leyendo el segundo archivo...:'); 
+fs.readFile('CNodeDC/archivo2.txt', 'utf-8', (err, data) => { // Leemos el archivo de forma asíncrona
+    if (err) {
+        console.error('Error al leer el archivo:', err);
+        return;
+    }
+    console.log(data);
+});
