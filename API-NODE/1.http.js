95a71ed974521b24fd08fb84e926f75a735e55fa
DavoDev-Hub <juanpajmz13@gmail.com>
2025-10-06 18:21:16 -0600

    refactor: eliminar código comentado y corregir nombre de archivo de imagen

--- API-NODE/1.http.js
+++ API-NODE/1.http.js
@@ -1,8 +1,6 @@
 const http = require('node:http')
 const fs = require('node:fs')
 
-console.log(process.env) // variables de entorno
-
 const desiredPort = process.env.PORT || 3000 // puerto por defecto o el que venga en las variables de entorno
 
 const processRequest = http.createServer((req, res) => {
@@ -11,9 +9,7 @@ const processRequest = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end('Welcome to the Home Page!!')
   } else if (req.url === '/Tuxito') {
-    res.statusCode = 200 // OK
-    res.setHeader('Content-Type', 'image/png')
-    fs.readFile('tuxito.png', (err, data) => {
+    fs.readFile('tuxito.jpg', (err, data) => {
       if (err) {
         res.statusCode = 500 // Internal Server Error
         res.end('Error loading image')
@@ -31,16 +27,8 @@ const processRequest = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end('404 Not Found')
   }
-  res.end('Hello from Node.js HTTP server!')
 })
 
 processRequest.listen(desiredPort, () => {
   console.log(`Server listening on port http://localhost:${desiredPort}`)
 })
-
-/*
-server.listen(0, () => {
-    const address = server.address()
-    console.log(`Server is listening on port ${address.port}`)
-}) // puerto aleatorio, no recomendable para produccion
-*/
