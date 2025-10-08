80a9b02272461543e77d31dabbea9ac9e28d1ff7
DavoDev-Hub <juanpajmz13@gmail.com>
2025-10-08 16:55:45 -0600

    Update main entry point and add new HTTP server implementation
    - Changed main entry point in package.json from "2.http.js" to "3.http.js"
    - Updated dev script in package.json to use "3.http.js"
    - Created a new HTTP server in "3.http.js" using Express
    - Implemented middleware to parse JSON request bodies
    - Added GET endpoint for fetching Ditto data
    - Added POST endpoint for receiving Pokémon data with a timestamp
    - Included a 404 error handler for undefined routes
    - Added Express as a dependency in package.json

---
+++ API-NODE/3.http.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const ditto = require('./pokemon/ditto.json')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+// middlewares
+app.use((req, res, next) => {
+  if (req.method !== 'POST') return next()
+  if (req.headers['content-type'] !== 'application/json') return next()
+
+  let body = ''
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    // mutar la request y meter la informacion en el req.body
+    req.body = data
+    next()
+  })
+})
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
+})
+
+app.post('/pokemon', (req, res) => {
+  let body = ''
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    res.status(201).json(data)
+  })
+})
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 Not Found</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
+})
