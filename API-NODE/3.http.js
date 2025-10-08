4f573ff3badaa282b778bf9335933714751fc742
DavoDev-Hub <juanpajmz13@gmail.com>
2025-10-08 17:00:16 -0600

    refactor: eliminar middleware comentado para el manejo de solicitudes POST

--- API-NODE/3.http.js
+++ API-NODE/3.http.js
@@ -4,25 +4,26 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.json()) // hace exactamente lo mismo que el middleware comentado más abajo
 // middlewares
-app.use((req, res, next) => {
-  if (req.method !== 'POST') return next()
-  if (req.headers['content-type'] !== 'application/json') return next()
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
 
-  let body = ''
-  // escuchar el evento data
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
+//   let body = ''
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
 
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    data.timestamp = Date.now()
-    // mutar la request y meter la informacion en el req.body
-    req.body = data
-    next()
-  })
-})
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la informacion en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
 
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
